7a010dbcdead2c10a071d9718ec4495aaaf381fd
Heman <gooogle5810@gmail.com>
2026-04-07 09:58:18 +0530

    starting the project

---
+++ TaskMgmtSystem/src/main/java/com/springboot/taskmgmtsystem/TaskMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.taskmgmtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ TaskMgmtSystem/src/test/java/com/springboot/taskmgmtsystem/TaskMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.taskmgmtsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
